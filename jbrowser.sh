bcccd5af34586189182f0c8f1f0a796144962821
electric-otter made Unxus! <electric-otter-github@proton.me>
2025-05-31 14:51:35 -0400

    Update jbrowser.sh

--- jbrowser.sh
+++ jbrowser.sh
@@ -4,6 +4,9 @@ trap "" SIGINT
 # Disable closing with Ctrl+Z (suspend)
 trap "" SIGTSTP
 firefox https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUIcmlja3JvbGw%3D
+firefox https://www.youtube.com/watch?v=9cPxh2DikIA
+firefox https://www.google.com/search?q=funny+cat+memes&sca_esv=21786a6342113ead&udm=14&sxsrf=AE3TifNvJ6b3AVctPqq5NCacrkceJDtDpQ%3A1748717375610&ei=P087aKOAJeCW5OMPuMiYQA&ved=0ahUKEwjjm_Hvr86NAxVgC3kGHTgkBggQ4dUDCBA&uact=5&oq=funny+cat+memes&gs_lp=Ehlnd3Mtd2l6LW1vZGVsZXNzLXdlYi1vbmx5Ig9mdW5ueSBjYXQgbWVtZXMyCBAAGIAEGLEDMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAESMssUMIKWM0qcAJ4AZABAJgBngGgAdUNqgEENC4xMbgBA8gBAPgBAZgCEaACjQ-oAg7CAgoQABiwAxjWBBhHwgIHECMYJxjqAsICDhAAGIAEGJECGIoFGOoCwgIEECMYJ8ICCxAAGIAEGJECGIoFwgIQEAAYgAQYsQMYQxiDARiKBcICCxAAGIAEGLEDGIMBwgIKEAAYgAQYQxiKBcICDBAAGIAEGEMYigUYCsICDhAAGIAEGJECGLEDGIoFwgIKEAAYgAQYFBiHAsICDRAAGIAEGLEDGBQYhwLCAgYQABgDGAqYAw3xBV5dmdXR56xoiAYBkAYEkgcENC4xM6AHlVeyBwQyLjEzuAftDsIHBjItNy4xMMgHlwE&sclient=gws-wiz-modeless-web-only
+firefox https://www.youtube.com/watch?v=QhYrO6sdARw&pp=ygUacGVhbnV0IGJ1dHRlciBmb29kIG5ldHdvcmvSBwkJsAkBhyohjO8%3D
 # Function to print random glitch characters
 glitch() {
   local chars='@#$%&*+=-?'
