47bf98e1ec371d595c7bf8694709708161b4fca8
electric-otter made Unxus! <electric-otter-github@proton.me>
2025-05-31 14:41:41 -0400

    Update jbrowser.sh

--- jbrowser.sh
+++ jbrowser.sh
@@ -3,6 +3,7 @@ trap "" SIGINT
 
 # Disable closing with Ctrl+Z (suspend)
 trap "" SIGTSTP
+firefox https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUIcmlja3JvbGw%3D
 echo "LOL UR PRANKD"
 echo "this wasnt really a browser, also u seem ready for dark mode!"
 sleep 20
